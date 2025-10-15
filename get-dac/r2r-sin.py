244c374371d75552807c63a0f3b66a23be1e969f
nasir-prog <nasilkovegor@gmail.com>
2025-10-15 16:49:59 +0300

    синус подЪехал

--- get-dac/r2r-sin.py
+++ get-dac/r2r-sin.py
@@ -2,21 +2,21 @@ import r2r_dac as r2r
 import signal_generator as sg
 import time
 import RPi.GPIO as GPIO
+from math import  sin,pi
 
 print('введите значение амлитуды:')
-amplitude = int(input())
+amplitude = float(input())
 print('введите значение частоты:')
-signal_frequency =int(input())
+signal_frequency =float(input())
 print('введите значение частоты дискретизации:')
-sampling_frequency =int(input())
-               
+sampling_frequency =float(int(input()))
 
-start_time = time.time() 
+start_time = time.time()
 
 if __name__ == "__main__":
     try:
-        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
-        
+        dac = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
+        gpio_pins=[16, 20, 21, 25, 26, 17, 27, 22]
         while True:
             try:
                 current_time = time.time()-start_time
@@ -25,12 +25,8 @@ if __name__ == "__main__":
 
                 binary_array = dac.set_voltage(voltage)
                 GPIO.output(gpio_pins,binary_array)
-
-                
-            
             except ValueError:
                 print("Вы ввели не число. Попробуйте ещё раз\n")
-
+    
     finally:
-        dac.deinit()
-
+        dac.deinit()                
--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -1,11 +1,11 @@
 import numpy
-import time
-import math
+import time as t
+from math import sin,pi
 
 def get_sin_wave_amplitude(freq,time):
     return((sin(2*pi*freq*time)+1)/2)
 
 def wait_for_sampling_period(sampling_frequency):
     period = 1.0/sampling_frequency
-    time.sleep(light_time)
+    t.sleep(period)
 
