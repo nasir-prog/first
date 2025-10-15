7d8b2fa56edaa8d163a2f1c1b8a09ccb666e86a4
nasir-prog <nasilkovegor@gmail.com>
2025-10-15 18:33:07 +0300

    sd

--- get-dac/supporter_for_triangle.py
+++ get-dac/supporter_for_triangle.py
@@ -5,8 +5,8 @@ from math import sin,pi
 def get_triangle_wave_amplitude(freq, time):
     period = 1.0 / freq  # Период в секундах
     position_in_period = time % period  # Позиция в текущем периоде
-    phase = position_in_period/2
-    if phase < period/2:
+    phase = position_in_period/period
+    if phase < 0.5:
         return phase
     else:
         return -1*phase
