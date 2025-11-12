eb866ad5ddcb658b4ca3eead9b7e752ecb01c3c8
Егор Насилков <nasilkovegor@gmail.com>
2025-11-12 18:18:28 +0300

    Update r2r-sin.py

--- get-dac/r2r-sin.py
+++ get-dac/r2r-sin.py
@@ -2,32 +2,25 @@ import r2r_dac as r2r
 import signal_generator as sg
 import time
 
-import RPi.GPIO as GPIO
-from math import  sin,pi
-
-print('введите значение амлитуды:')
-amplitude = float(input())
-print('введите значение частоты:')
-signal_frequency =float(input())
-print('введите значение частоты дискретизации:')
-sampling_frequency =float(int(input()))
-
-start_time = time.time()
+amplitude = 3.17
+signal_frequency = 10
+sampling_frequency = 1000
 
 if __name__ == "__main__":
     try:
-        dac = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
-        gpio_pins=[16, 20, 21, 25, 26, 17, 27, 22]
+        dac = r2r.R2R_DAC([16,20,21,25,26,17,27,22], 3.17, True)
+
         while True:
             try:
-                current_time = time.time()-start_time
-                sg.wait_for_sampling_period(sampling_frequency)
-                voltage  = sg.get_sin_wave_amplitude(signal_frequency,current_time)
+                signal_amplitude = sg.get_sin_wave_amplitude(signal_frequency, time.time())
+                voltage = signal_amplitude * amplitude
+                dac.set_voltage(voltage)
+                time.sleep(sg.wait_for_sampling_period(sampling_frequency))
 
-                binary_array = dac.set_voltage(voltage)
-                GPIO.output(gpio_pins,binary_array)
             except ValueError:
-                print("Вы ввели не число. Попробуйте ещё раз\n")
-    
+                print("Ошибка значения. Попробуйте еще раз\n")
+            except KeyboardInterrupt:
+                print("\nПрограмма завершена")
+                break
     finally:
-        dac.deinit()                
+        dac.deinit()
